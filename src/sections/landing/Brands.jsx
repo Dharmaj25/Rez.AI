5084686a94bc8422081a6a13104cc1b83646897f
Dharmaj <paniyadharmaj@gmail.com>
2025-10-13 12:01:50 +0530

    feat: made Brands section responsive for mobile,tablet and desktop screens

--- src/sections/landing/Brands.jsx
+++ src/sections/landing/Brands.jsx
@@ -1,5 +1,4 @@
 import googlePng from "../../assets/brands/google.png";
-import msLogo from "../../assets/brands/microsoft.png";
 import netflixLogo from "../../assets/brands/netflix.png";
 import spotifyLogo from "../../assets/brands/spotify.png";
 import atlassianLogo from "../../assets/brands/atlassian.png";
@@ -23,20 +22,20 @@ const logos = [
 
 const Brands = () => {
     return (
-        <section className="py-16 animate-drop-custom my-24">
+        <section className="lg:py-16 animate-drop-custom my-24">
             <div className="max-w-6xl mx-auto text-center">
-                <div className="mb-12 text-center">
-                    <h1 className="text-5xl font-extrabold text-gray-700 mb-3">
+                <div className="mb-12 text-center pb-1">
+                    <h1 className="text-4xl lg:text-5xl font-extrabold text-gray-700 mb-3 leading-[1.25]">
                         Join thousands of candidates hired at
                     </h1>
                     <h1
-                        className="text-5xl font-extrabold 
-             bg-gradient-to-r from-blue-400 via-blue-500 to-blue-800 
-             bg-clip-text text-transparent tracking-tight
-             leading-[1.3]"
+                        className="text-4xl lg:text-5xl font-extrabold 
+    bg-gradient-to-r from-blue-400 via-blue-500 to-blue-800 
+    bg-clip-text text-transparent tracking-tight leading-[1.25] overflow-visible lg:leading-[1.5]"
                     >
                         leading global brands
-                    </h1>                </div>
+                    </h1>
+                </div>
                 {/* Grid for logos */}
                 <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 place-items-center">
                     {logos.map((logo, index) => (
@@ -48,7 +47,7 @@ const Brands = () => {
                                 src={logo}
                                 alt="brand logo"
                                 className="
-                  h-16 sm:h-20 max-w-[150px] object-contain
+                  h-10 sm:h-20 max-w-[150px] object-contain
                   grayscale transition duration-500 ease-out
                   group-hover:grayscale-0 group-hover:scale-110
                 "
