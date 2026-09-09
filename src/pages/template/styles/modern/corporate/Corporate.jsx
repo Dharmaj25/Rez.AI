a5f391da27f2c8cc97687ddb5c1a0505dc2f2e47
Dharmaj25 <paniyadharmaj@gmail.com>
2026-09-09 18:12:36 +0530

    feat(ui): update parent component structure for modern template 2

--- src/pages/template/styles/modern/corporate/Corporate.jsx
+++ src/pages/template/styles/modern/corporate/Corporate.jsx
@@ -4,6 +4,7 @@ import Experience from "./Experience";
 import Header from "./Header";
 import Skills from "./Skills";
 import Summary from "./Summary";
+import Language from "./Language";
 
 import { demoData, visibleSections } from "./sampleData";
 
@@ -17,13 +18,14 @@ const Corporate = ({ data, sections }) => {
             {supportedSections.includes("header") && <Header data={templateData} />}
             <div className="grid grid-cols-10 p-6 gap-10">
                 <div className="col-span-3 flex flex-col gap-10">
-                    <Education data={templateData} />
-                    <Skills data={templateData} />
+                    {supportedSections.includes("education") && <Education data={templateData} />}
+                    {supportedSections.includes("skills") && <Skills data={templateData} />}
+                    {supportedSections.includes("language") && <Language data={templateData} />}
                 </div>
                 <div className="col-span-7 flex flex-col gap-6">
-                    <Summary data={templateData} />
-                    <Experience data={templateData} />
-                    <Certification data={templateData}/>
+                    {supportedSections.includes("summary") && <Summary data={templateData} />}
+                    {supportedSections.includes("experience") && <Experience data={templateData} />}
+                    {supportedSections.includes("certification") && <Certification data={templateData} />}
                 </div>
             </div>
         </div>
